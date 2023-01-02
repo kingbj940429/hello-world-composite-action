d721a547288cb3e357e6cf97013a2bc39ecdcc53
kingbj0429 <kingbj0429@lunit.io>
2023-01-02 13:53:25 +0900

    Create Action

--- git-push.sh
+++ git-push.sh
@@ -4,5 +4,5 @@ git add .
 git commit -m "Create Action"
 git push origin main
 
-git tag -a -m "Description of this release" v4
+git tag -a -m "Description of this release" v6
 git push --follow-tags
